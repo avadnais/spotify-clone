2b206342a1a50423a5a406a657a43f5c33eb8e42
Andy <avadnais@oswego.edu>
2022-01-20 12:29:02 -0500

    Updates

---
+++ precache-manifest.70ee2f57a5921756c60831b202d7458f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e5b9db36a339c0094ff79a921db23e",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "84a46e29ff67d30fc210",
+    "url": "/spotify-clone/static/css/main.79b9caea.chunk.css"
+  },
+  {
+    "revision": "2a513cb3554ac665d471",
+    "url": "/spotify-clone/static/js/2.6e512b2b.chunk.js"
+  },
+  {
+    "revision": "7f807c06332800735dbd",
+    "url": "/spotify-clone/static/js/3.812b1523.chunk.js"
+  },
+  {
+    "revision": "84a46e29ff67d30fc210",
+    "url": "/spotify-clone/static/js/main.9384acf6.chunk.js"
+  },
+  {
+    "revision": "778da50e2dc5ad1893ab",
+    "url": "/spotify-clone/static/js/runtime~main.5bae904c.js"
+  }
+]);
